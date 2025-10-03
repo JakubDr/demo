f02b5c569233d575e9bcee58c20bf48975fb97a1
Jakub Drastík <it2403@sspu-opava.cz>
2025-10-03 08:10:04 +0200

    start

---
+++ demo/js/kouzla.js
@@ -0,0 +1,39 @@
+/* deklarace konstant, ktere odkazuji na prvky webove stranky podle ID */
+const fotka = document.getElementById("fotka");
+const kouzelnyText = document.getElementById("kouzelnyText");
+const dec = document.getElementById("dec");
+const convert = document.getElementById("convert");
+const result = document.getElementById("result");
+
+/* Funkce pro vygenerovani nahodne barvy podle RGB */
+function randomColour() {
+    /* r, g, b - promenne pro jednotlive barevne slozky 
+    Math. round() - vestavena funkce, zaokrouhluje
+    Math.random() - cislo od 0 do 1 nahodne generovane*/
+            let r = Math.round(Math.random() * 255); 
+            let g = Math.round(Math.random() * 255);
+            let b = Math.round(Math.random() * 255);
+            return `rgb(${r}, ${g}, ${b})`;
+        }
+        fotka.addEventListener("mouseover", function() {
+            fotka.width *= 2
+        }); 
+        fotka.addEventListener("mouseout", function() {
+            fotka.width /= 2
+        });
+        kouzelnyText.addEventListener("click", function() {
+    let txt = prompt('Zadej text');
+    kouzelnyText.innerHTML = txt ? txt : 'Zadej text:';
+    kouzelnyText.style.color = randomColour();
+    let x = Math.round(Math.random() * 40 + 10);
+    kouzelnyText.style.fontSize = x + "px";
+    kouzelnyText.innerHTML += x;
+});
+
+convert.addEventListener("click", function() {
+    let decNumber = parseInt(dec.value);
+    console.log(decNumber.toString(16));
+    result.innerHTML = ` <b>BIN: ${decNumber.toString(2)} <br>
+                        <b> OCT: ${decNumber.toString(8)} <br>
+                        <b> HEX: ${decNumber.toString(16)} <br>`;
+});
